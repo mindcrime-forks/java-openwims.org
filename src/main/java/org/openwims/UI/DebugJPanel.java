a01f1b7dee94ac250f954e431c80bbe6794a89bc
jesse <jesse@zarek.home>
2013-11-11 08:51:28 -0500

    fix for java 6 compatibility

--- src/main/java/org/openwims/UI/DebugJPanel.java
+++ src/main/java/org/openwims/UI/DebugJPanel.java
@@ -119,7 +119,7 @@ public class DebugJPanel extends JPanel {
     private class SensesListCellRenderer extends DefaultListCellRenderer {
 
         @Override
-        public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+        public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
             JLabel label = (JLabel)super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
             if (value instanceof Sense) {
                 Sense sense = (Sense) value;
